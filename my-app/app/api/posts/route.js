3a1cd11d8f18db2faee3a5274d3baeac0df5c2b9
Inferno-coder <pradeepceo18@gmail.com>
2024-02-08 16:31:01 +0530

    update

--- my-app/app/api/posts/route.js
+++ my-app/app/api/posts/route.js
@@ -2,10 +2,20 @@ import connectMongo from '../../../utils/connectMongo';
 import postModel from '../../../utils/models/postModel';
 const mongoose=require('mongoose');
 
-export async function GET(){
+export async function GET(req,{params}){
+    const query=req.nextUrl.searchParams.get('q')
 try {
     await connectMongo()
-    const posts=await postModel.find({})
+    let posts;
+    if(query){
+        posts=await postModel.find({
+            $or:[
+            {title: new RegExp(query,'i')},
+            {desc: new RegExp(query,'i')},
+            ]   
+        });
+    }
+    else posts=await postModel.find({})
     return Response.json(posts)
 } catch (error) {
     return Response.json({msg:error.message})
--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,13 +1,27 @@
 "use client"
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Link  from "next/link";
 export default function Home() {
+   
    const [posts,setPosts]=useState([])
+   const [search,setSearch]=useState(false)
+   const [searchValue,setSearchValue]=useState('')
    useEffect(()=>{
     fetch(process.env.NEXT_PUBLIC_API_URL+'/posts')
     .then((res)=>res.json())
     .then((res)=>setPosts(res))
    },[])
+ 
+  const searchPost=(e)=>{
+     if(e.type=='keydown' && e.key!=='Enter')return
+    setSearch(true)
+    fetch(process.env.NEXT_PUBLIC_API_URL+'/posts?q='+searchValue)
+    .then((res)=>res.json())
+    .then((res)=>setPosts(res))
+    .then(()=>setSearch(false))
+    setSearchValue("")
+  }
+
   return (
     <>
       <main className="container mx-auto px-4 py-6">
@@ -16,8 +30,8 @@ export default function Home() {
       </main>
 
       <div className="flex justify-end px-4">
-          <input type="text" className="px-4 py-2 border border-gray-300 rounded-md" placeholder="Search..." />
-          <button className="px-4 py-2 bg-blue-500 text-white rounded-md ml-4">Search</button>
+          <input  onKeyDown={searchPost} value={searchValue} onChange={(e)=>setSearchValue(e.target.value)} type="text" className="px-4 py-2 border border-gray-300 rounded-md" placeholder="Search..." />
+          <button onClick={searchPost} disabled={search} className="px-4 py-2 bg-blue-500 text-white rounded-md ml-4">{search?'...':'Search'}</button>
         </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {posts.map((post)=>(
@@ -30,6 +44,7 @@ export default function Home() {
             </Link>
           
           ))}
+          {posts.length<=0  && <h1 style={{fontSize:'40px'}}>No posts are available for this search</h1>}
       </div>
     </>
   );
